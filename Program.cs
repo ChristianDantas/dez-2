97173bba2d65021fd807788c0711b79c09012487
Christian <christian_d_ol9000@hotmail.com>
2020-12-02 16:35:06 -0300

    acabado

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Atividade_atributos.Classes;
+
+namespace Atividade_atributos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta2;
+            Aluno aluno1 = new Aluno();
+           do{
+
+            Console.Clear();
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno1.nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Insira o Curso: ");
+            aluno1.curso = Console.ReadLine();
+
+            Console.WriteLine("Insira a idade do aluno: ");
+            aluno1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o RG do Aluno");
+            aluno1.RG = Console.ReadLine();
+
+            Console.WriteLine("O aluno é bolsista? (S/N)");
+            string resposta = Console.ReadLine();
+            if (resposta.ToLower() == "s") {
+                aluno1.bolsista = true;
+            } else if (resposta.ToLower() == "n") {
+                aluno1.bolsista = false;
+            }
+
+            Console.WriteLine("insira a média do aluno: ");
+            aluno1.mediaFinal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira o valor da mensalidade: ");
+            aluno1.valorDaMensalidade = double.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine(aluno1.nomeCompleto);
+            Console.WriteLine(aluno1.curso);
+            Console.WriteLine(aluno1.idade);
+            Console.WriteLine(aluno1.RG);
+            Console.WriteLine(aluno1.bolsista);
+            aluno1.VerMediaFinal(aluno1.mediaFinal);
+            aluno1.VerMensalidade(aluno1.bolsista, aluno1.valorDaMensalidade);
+            Console.WriteLine("gostaria de registrar outro aluno? S/N");
+             resposta2=Console.ReadLine();
+           } while(resposta2 == "s");
+        }
+    }
+}
